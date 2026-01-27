b4489f70e0bf18d5f51b136f7e8243aac8f01fd1
Moses Kariuki <kariukifx471@gmail.com>
2026-01-27 23:09:04 +0300

    Intro to Databases: schema and task scripts

---
+++ alx_book_store.sql
@@ -0,0 +1,26 @@
+CREATE TABLE authors(author_id AUTO_INCREMENT PRIMARY KEY ,
+ author_name VARCHAR(215));
+
+CREATE TABLE customers(customer_id INT AUTO_INCREMENT PRIMARY KEY ,
+ customer_name VARCHAR(215) NOT NULL ,
+  email VARCHAR(215) ,
+   address TEXT);
+
+CREATE TABLE books(book_id INT AUTO_INCREMENT PRIMARY KEY ,
+ title VARCHAR(130) NOT NULL ,
+  author_id INT NOT NULL ,
+  price DOUBLE ,
+   publication_date DATE ,
+    FOREIGN KEY(author_id) REFERENCES authors(author_id));
+
+CREATE TABLE orders(order_id INT AUTO_INCREMENT PRIMARY KEY ,
+ customer_id INT NOT NULL  ,
+  order_date DATE ,
+   FOREIGN KEY(customer_id) REFERENCES customers(customer_id));
+
+CREATE TABLE order_details(orderdetailid INT AUTO_INCREMENT PRIMARY KEY ,
+ order_id INT NOT NULL ,
+  book_id INT NOT NULL ,
+   quantity DOUBLE ,
+    FOREIGN KEY(order_id) REFERENCES orders(order_id) ,
+     FOREIGN KEY(book_id) REFERENCES books(book_id));
